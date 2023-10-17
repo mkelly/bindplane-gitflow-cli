35c49fe9cb6eca96ff459434c4e8bc2722fb3a2f
Michael Kelly <michael.e.kelly@gmail.com>
2023-10-17 17:58:18 -0400

    mkdir

--- apply_bindplane_configs.sh
+++ apply_bindplane_configs.sh
@@ -3,5 +3,7 @@
 # Directory to start the search; default is the current directory
 start_directory="./bindplane"
 
+mkdir -p "${start_directory}"
+
 # Use find command to locate all yaml files and execute bindplane apply on each
 find "$start_directory" -name "*.yaml" -type f -exec echo "Applying {}" \; -exec bindplane apply -f {} \;
