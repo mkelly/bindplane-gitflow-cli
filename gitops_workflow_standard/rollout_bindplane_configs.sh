06ef820cf94231614dade53e15e72070bd107212
Michael Kelly <michael.e.kelly@gmail.com>
2023-10-19 12:28:51 -0400

    Rollout action

---
+++ gitops_workflow_standard/rollout_bindplane_configs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directory to start the search; default is the current directory
+start_directory="./bindplane/configuration"
+
+mkdir -p "${start_directory}"
+
+bindplane_output=$(bindplane get configs")
+
+# Parse the NAME field from the output and loop through each one
+echo "$bindplane_output" | awk 'NR>1 {print $1}' | while read -r name; do
+    bindplane rollout start "$name"
+done
+
+echo "Configuration rollout started."
+
